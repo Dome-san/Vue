9136c059be4f1e3c5f37afc247340212a79a9737
Dome-san <dimasmdv.seo@gmail.com>
2024-12-20 16:42:02 +0100

    creando chat, composables, helpers e interfaces

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
-  <hr />
-  <MyCounter :value="1" />
-  <MyCounterScipt :value="2" />
-  <MyCounterScipt2 :value="3" />
+  <IndecisionView />
 </template>
 
 <script lang="ts" setup>
-import MyCounter from './components/MyCounter.vue';
-import MyCounterScipt from './components/MyCounterScript.vue';
-import MyCounterScipt2 from './components/my-counter-script/MyCounterScript2.vue';
+import IndecisionView from './views/IndecisionView.vue';
 </script>
