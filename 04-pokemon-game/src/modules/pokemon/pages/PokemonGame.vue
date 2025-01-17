9d007f2df91067d7aa63e2b5744134c71c0ada8a
Dome-san <dimasmdv.seo@gmail.com>
2025-01-17 10:43:23 +0100

    creacion de interfaces, logica composable usePokemon y pokemonApi

--- 04-pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
+++ 04-pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
@@ -1,10 +1,11 @@
 <template>
-  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="text-3xl">Espere por favor</h1>
     <h3 class="animate-pulse">Cargando pokemons</h3>
   </section>
-  <section class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">Quien es este pokemon?</h1>
+    <h3>{{ randomPokemon }}</h3>
 
     <!-- Pokemon Pictures -->
     <PokemonPictures />
@@ -16,4 +17,7 @@
 <script setup lang="ts">
 import PokemonOptions from '../components/PokemonOptions.vue';
 import PokemonPictures from '../components/PokemonPictures.vue';
+import { usePokemonGame } from '../composables/usePokemonGame';
+
+const { randomPokemon, isLoading } = usePokemonGame();
 </script>
