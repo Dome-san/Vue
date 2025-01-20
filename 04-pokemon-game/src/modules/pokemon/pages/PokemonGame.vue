d5808a877cc224a36669ea0a583f7d554b4d2c10
Dome-san <dimasmdv.seo@gmail.com>
2025-01-20 12:15:44 +0100

    configurando logica app, configurando las opciones, pokemon pictures, actualizando paths

--- 04-pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
+++ 04-pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
@@ -1,16 +1,35 @@
 <template>
-  <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section
+    v-if="isLoading || randomPokemon.id === null"
+    class="flex flex-col justify-center items-center w-screen h-screen"
+  >
     <h1 class="text-3xl">Espere por favor</h1>
     <h3 class="animate-pulse">Cargando pokemons</h3>
   </section>
   <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">Quien es este pokemon?</h1>
-    <h3>{{ randomPokemon }}</h3>
+    <div class="h-20">
+      <button
+        v-if="gameStatus !== GameStatus.Playing"
+        @click="getNextRound(4)"
+        class="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-800 transition-all"
+      >
+        Prueba otra vez
+      </button>
+    </div>
 
     <!-- Pokemon Pictures -->
-    <PokemonPictures />
+    <PokemonPictures
+      :pokemon-id="randomPokemon.id"
+      :show-pokemon="gameStatus !== GameStatus.Playing"
+    />
     <!-- Pokemon Options -->
-    <PokemonOptions />
+    <PokemonOptions
+      :options="options"
+      :block-selection="gameStatus !== GameStatus.Playing"
+      :correctAnswer="randomPokemon.id"
+      @selected-option="checkAnswer"
+    />
   </section>
 </template>
 
@@ -18,6 +37,14 @@
 import PokemonOptions from '../components/PokemonOptions.vue';
 import PokemonPictures from '../components/PokemonPictures.vue';
 import { usePokemonGame } from '../composables/usePokemonGame';
+import { GameStatus } from '../interfaces';
 
-const { randomPokemon, isLoading } = usePokemonGame();
+const {
+  randomPokemon,
+  isLoading,
+  gameStatus,
+  pokemonOptions: options,
+  checkAnswer,
+  getNextRound,
+} = usePokemonGame();
 </script>
