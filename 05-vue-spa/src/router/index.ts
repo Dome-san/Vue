81c7f574b073ba6d527bd94f5607638f52506581
Dome-san <dimasmdv.seo@gmail.com>
2025-01-27 12:10:16 +0100

    Creando nuevo proyecto, creando estructura base, tailwind, Router, configuracion componentes hijos y padres en auth y landing

---
+++ 05-vue-spa/src/router/index.ts
@@ -0,0 +1,55 @@
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
+import HomePage from '@/modules/landing/pages/HomePage.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('@/modules/landing/template/LandingTemplate.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomePage,
+        },
+        {
+          path: '/features',
+          name: 'features',
+          component: () => import('@/modules/landing/pages/FeaturesPage.vue'),
+        },
+        {
+          path: '/pricing',
+          name: 'pricing',
+          component: () => import('@/modules/landing/pages/PricingPage.vue'),
+        },
+        {
+          path: '/contact',
+          name: 'contact',
+          component: () => import('@/modules/landing/pages/ContactPage.vue'),
+        },
+      ],
+    },
+    //Auth
+    {
+      path: '/auth',
+      redirect: { name: 'login' },
+      component: () => import('@/modules/auth/template/AuthTemplate.vue'),
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('@/modules/auth/pages/LoginPage.vue'),
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('@/modules/auth/pages/RegisterPage.vue'),
+        },
+      ],
+    },
+  ],
+});
+
+export default router;
