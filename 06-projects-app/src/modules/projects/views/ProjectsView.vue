71b11e7750bb7c104171a4052aa42dc8b8e3d204
Dome-san <dimasmdv.seo@gmail.com>
2025-01-30 13:35:31 +0100

    Creando botton con Slot

---
+++ 06-projects-app/src/modules/projects/views/ProjectsView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="overflow-x-auto w-full">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th></th>
+          <th>Proyecto</th>
+          <th>Tareas</th>
+          <th>Avance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="hover">
+          <th>2</th>
+          <td>Hart Hagerty</td>
+          <td>Desktop Support Technician</td>
+          <td>Purple</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <FabBotton @click="() => console.log('clicked')">
+    <AddCircle />
+  </FabBotton>
+</template>
+
+<script setup lang="ts">
+import FabBotton from '@/modules/common/components/FabBotton.vue';
+import AddCircle from '@/modules/common/icons/AddCircle.vue';
+</script>
