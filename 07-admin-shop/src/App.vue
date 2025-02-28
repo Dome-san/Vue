e72cd5d5240daf881a76f71a1349770629d494cc
Dome-san <dimasmdv.seo@gmail.com>
2025-02-28 12:18:02 +0100

    variables de entorno(Axios), obtener los productos de back, TanStack Query, url de las imagenes, mostrar productos en pantalla, paginacion y su funcionalidad, carga adelantada

--- 07-admin-shop/src/App.vue
+++ 07-admin-shop/src/App.vue
@@ -1,4 +1,9 @@
 <template>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <RouterView />
+  <VueQueryDevtools />
 </template>
+
+<script setup lang="ts">
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
+</script>
---
+++ 07-admin-shop/src/modules/products/components/ProductList.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="py-10 bg-gray-100">
+    <div
+      class="mx-auto grid max-w-6xl grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+    >
+      <!-- ProductCard => Product -->
+      <ProductCard v-for="product in products" :key="product.id" :product="product" />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import type { Producto } from '../interfaces/product.interface';
+import ProductCard from './ProductCard.vue';
+
+interface Props {
+  products: Producto[];
+}
+
+defineProps<Props>();
+</script>
